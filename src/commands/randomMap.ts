751c930f683dde17ec6ce5643f3cd36fd0304e4a
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-08 21:44:10 +0900

    feat: Add option to select game type in randomMap command

--- src/commands/randomMap.ts
+++ src/commands/randomMap.ts
@@ -1,12 +1,21 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 async function handler(interaction: ChatInputCommandInteraction) {
-    await interaction.reply("Pong!")
+    const targetMapId = interaction.options.getString("게임 종류");
+    console.log(targetMapId);
 }
 
 export default {
     info: new SlashCommandBuilder()
         .setName("랜덤맵")
-        .setDescription("[ 🗺️ ] 맵을 랜덤으로 선택해줍니다"),
+        .setDescription("[ 🗺️ ] 맵을 랜덤으로 선택해줍니다")
+        .addStringOption(option => 
+            option
+                .setName("게임 종류")
+                .setDescription("게임 종류를 선택해주세요")
+                .setRequired(true)
+                .setAutocomplete(true)
+        )
+    ,
     handler
 }
