0a83c31d6f34b172ca60ea3eefc0ddec3b7b2cd5
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-08 22:46:58 +0900

    feat: Add new Valorant map to SupportMapList constant

---
+++ src/constant/map.ts
@@ -0,0 +1,61 @@
+import { SupportGame, SupportMapList } from "../types/constant";
+
+const SupportMapList: SupportMapList[] = [
+    {
+        name: '어비스 (Abyss)',
+        value: 'Abyss',
+        game: SupportGame.Valorant,
+    },
+    {
+        name: '아이스박스 (Icebox)',
+        value: 'Icebox',
+        game: SupportGame.Valorant,
+    },
+    {
+        name: '어센트 (Ascent)',
+        value: 'Ascent',
+        game: SupportGame.Valorant,
+    },
+    {
+        name: '스플릿 (Split)',
+        value: 'Split',
+        game: SupportGame.Valorant,
+    },
+    {
+        name: '헤이븐 (Haven)',
+        value: 'Haven',
+        game: SupportGame.Valorant,
+    },
+    {
+        name: '브리즈 (Breeze)',
+        value: 'Breeze',
+        game: SupportGame.Valorant,
+    },
+    {
+        name: '프랙쳐 (Fracture)',
+        value: 'Fracture',
+        game: SupportGame.Valorant,
+    },
+    {
+        name: '바인드 (Bind)',
+        value: 'Bind',
+        game: SupportGame.Valorant,
+    },
+    {
+        name: '로터스 (Lotus)',
+        value: 'Lotus',
+        game: SupportGame.Valorant,
+    },
+    {
+        name: '펄 (Pearl)',
+        value: 'Pearl',
+        game: SupportGame.Valorant,
+    },
+    {
+        name: '선셋 (Sunset)',
+        value: 'Sunset',
+        game: SupportGame.Valorant,
+    }
+]
+
+export default SupportMapList;
