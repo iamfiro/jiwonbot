800bbe933591bf59932cf070b47738066a18b348
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-11 08:23:20 +0900

    feat: Add support for Valorant and PUBG maps

--- src/constant/game.ts
+++ src/constant/game.ts
@@ -4,6 +4,14 @@ const SupportGameList: SupportGameList[] = [
     {
         name: '🔫 발로란트',
         value: SupportGame.Valorant
+    },
+    {
+        name: '배틀그라운드 - 일반맵',
+        value: SupportGame["PUBG Default"]
+    },
+    {
+        name: '배틀그라운드 - 사설맵',
+        value: SupportGame["PUBG Custom"]
     }
 ]
 
