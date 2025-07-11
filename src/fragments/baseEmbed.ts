db903f21411b402bd51362b0cfb44e8289a88347
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-07-12 00:29:09 +0900

    feat: implement baseEmbed class for Custom Embed

---
+++ src/fragments/baseEmbed.ts
@@ -0,0 +1,33 @@
+import { getGuildLanguage } from "@/database/language";
+import { createLangFunction, SupportedLanguage } from "@/utils/language";
+
+export abstract class BaseEmbed {
+    protected guildId: string;
+    protected language: SupportedLanguage;
+    protected t: (key: string) => string;
+    private _initialize: boolean = false;
+
+    constructor(guildId: string) {
+        this.guildId = guildId;
+        this.language = 'en';
+        this.t = createLangFunction(this.language)
+    }
+
+    private async initialize(): Promise<void> {
+        if(this._initialize) return;
+
+        this.language = await getGuildLanguage(this.guildId)
+        this.t = await createLangFunction(this.language);
+        this._initialize = true;
+    }
+
+    protected async ensureInitialized(): Promise<void> {
+        await this.initialize()
+    }
+
+    updateLanguage(language: SupportedLanguage): void {
+        this.language = language;
+        this.t = createLangFunction(language);
+        this._initialize = true;
+    }
+}
