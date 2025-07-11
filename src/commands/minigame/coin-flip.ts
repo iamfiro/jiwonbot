081251934edf08a614aa6452dcfa2e6bf56cac06
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-07-11 19:53:27 +0900

    feat: implement coin-flip command with localized descriptions and interactive response

--- src/commands/minigame/coin-flip.ts
+++ src/commands/minigame/coin-flip.ts
@@ -1,10 +1,49 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+	ChatInputCommandInteraction,
+	Colors,
+	EmbedBuilder,
+	SlashCommandBuilder,
+} from 'discord.js';
 
 export default {
-    info: new SlashCommandBuilder()
-        .setName('flip')
-        .setDescription('Replies with Pong!'),
-    execute: async (interaction: ChatInputCommandInteraction) => {
-        await interaction.reply('Pong!');
-    }
-}
+	info: new SlashCommandBuilder()
+		.setName('coin-flip')
+		.setNameLocalizations({
+			ko: '동전던지기',
+		})
+		.setDescription("Flips a coin and tells you if it's heads or tails!")
+		.setDescriptionLocalizations({
+			ko: '동전을 던져서 앞면인지 뒷면인지 알려드려요!',
+		}),
+	execute: async (interaction: ChatInputCommandInteraction) => {
+		const coin = Math.floor(Math.random() * 2) == 0 ? 'heads' : 'tails';
+
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle('Flipping a coin...')
+                    .setColor(Colors.Yellow)
+                    .setFooter({
+                        text: 'Please wait...',
+                        iconURL: interaction.user.displayAvatarURL(),
+                    }),
+            ]
+        });
+
+        // 1.5 seconds delay to simulate coin flip
+        await new Promise(resolve => setTimeout(resolve, 1500));
+
+		await interaction.editReply({
+			embeds: [
+				new EmbedBuilder()
+					.setTitle(`🪙 ${coin} !`)
+					.setDescription('Coin Flip')
+					.setColor(Colors.Yellow)
+                    .setFooter({
+                        text: `Flipped by ${interaction.user.tag}`,
+                        iconURL: interaction.user.displayAvatarURL(),
+                    }),
+			],
+		});
+	},
+};
