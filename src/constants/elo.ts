915ee252740622f719313bc3557188ec2100ce67
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-07-14 11:21:32 +0900

    feat: reduce elo values

---
+++ src/constants/elo.ts
@@ -0,0 +1,22 @@
+export const valorantTierPoints: Record<string, number> = {
+    'Iron 1': 100, 'Iron 2': 170, 'Iron 3': 240,
+    'Bronze 1': 320, 'Bronze 2': 410, 'Bronze 3': 500,
+    'Silver 1': 650, 'Silver 2': 780, 'Silver 3': 910,
+    'Gold 1': 1100, 'Gold 2': 1280, 'Gold 3': 1460,
+    'Platinum 1': 1700, 'Platinum 2': 1950, 'Platinum 3': 2200,
+    'Diamond 1': 2550, 'Diamond 2': 2950, 'Diamond 3': 3400,
+    'Ascendant 1': 4000, 'Ascendant 2': 4650, 'Ascendant 3': 5350,
+    'Immortal 1': 6200, 'Immortal 2': 7100, 'Immortal 3': 8100,
+    'Radiant': 10000,
+}
+
+export const lolTierPoints: Record<string, number> = {
+    'Iron IV': 100, 'Iron III': 170, 'Iron II': 240, 'Iron I': 310,
+    'Bronze IV': 400, 'Bronze III': 490, 'Bronze II': 580, 'Bronze I': 670,
+    'Silver IV': 780, 'Silver III': 890, 'Silver II': 1000, 'Silver I': 1110,
+    'Gold IV': 1250, 'Gold III': 1380, 'Gold II': 1510, 'Gold I': 1640,
+    'Platinum IV': 1800, 'Platinum III': 1950, 'Platinum II': 2100, 'Platinum I': 2250,
+    'Emerald IV': 2450, 'Emerald III': 2650, 'Emerald II': 2850, 'Emerald I': 3050,
+    'Diamond IV': 3350, 'Diamond III': 3650, 'Diamond II': 3950, 'Diamond I': 4300,
+    'Master': 5000, 'Grandmaster': 6500, 'Challenger': 8500
+};
